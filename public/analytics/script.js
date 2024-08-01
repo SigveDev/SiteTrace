608c6b5ee5a15a83a09164152114efd0ba4b3b18
2derp4you <iam2derp4you@gmail.com>
2024-08-01 23:10:25 +0200

    fixed timeezone in timestamp in script.js

--- public/analytics/script.js
+++ public/analytics/script.js
@@ -75,12 +75,38 @@
     };
   }
 
+  function toIsoString(date) {
+    var tzo = -date.getTimezoneOffset(),
+      dif = tzo >= 0 ? "+" : "-",
+      pad = function (num) {
+        return (num < 10 ? "0" : "") + num;
+      };
+
+    return (
+      date.getFullYear() +
+      "-" +
+      pad(date.getMonth() + 1) +
+      "-" +
+      pad(date.getDate()) +
+      "T" +
+      pad(date.getHours()) +
+      ":" +
+      pad(date.getMinutes()) +
+      ":" +
+      pad(date.getSeconds()) +
+      dif +
+      pad(Math.floor(Math.abs(tzo) / 60)) +
+      ":" +
+      pad(Math.abs(tzo) % 60)
+    );
+  }
+
   function sendAnalyticsData(isFullData = false) {
     let payload = {
       url: window.location.href,
       referrer: document.referrer,
       sessionId: getSessionId(),
-      timestamp: new Date().toISOString(),
+      timestamp: toIsoString(new Date()),
       browser: {
         name: getBrowserName(),
         version: getBrowserVersion(),
